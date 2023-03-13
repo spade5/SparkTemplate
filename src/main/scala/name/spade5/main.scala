8a0cd5d82596b46f4351a6a9fe942b27c17dd6b0
spade5 <spade5@spade5.name>
2023-03-13 16:42:49 +0800

    streaming

--- src/main/scala/name/spade5/main.scala
+++ src/main/scala/name/spade5/main.scala
@@ -1,25 +1,24 @@
 package name.spade5
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object Main {
-  def main(args: Array[String]) = {
-    System.setProperty("HADOOP_USER_NAME", "root")
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("FileWordCount").setMaster("local[2]")
+    val ssc = new StreamingContext(conf, Seconds(5))
 
-    val conf = new SparkConf().setAppName("RealtimeEtl").setMaster("local[*]")
-    val spark = SparkSession.builder().config(conf).getOrCreate()
+    val lines = ssc.textFileStream("/workspace/novel.txt")
+    val words = lines.flatMap(_.split(" "))
+    val wordCounts = words.map((_, 1)).reduceByKey(_ + _)
 
-    val rdd = spark.sparkContext.textFile("mockDAUData.txt")
+    wordCounts.foreachRDD(rdd => {
+      if (!rdd.isEmpty()) {
+        rdd.saveAsTextFile("/workspace/streaming-result/output-" + System.currentTimeMillis() + ".txt")
+      }
+    })
 
-    val res = rdd.map((line: String) => {
-      val splits = line.split(",")
-      ((splits(0), splits(1)), 1)
-    }).reduceByKey((a, b) => 1) //日期，用户 去重
-      .map((tuple) => (tuple._1._1, 1))  //统计去重的用户数
-      .reduceByKey(_+_)
-
-    res.foreach(println)
-    res.saveAsTextFile("hdfs://hadoop0:9000/user/root/DAU_data")
+    ssc.start()
+    ssc.awaitTermination()
   }
 }
