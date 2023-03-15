0c5ac33cf49b4fdb87c58d1f22418db85709692a
spade5 <spade5@spade5.name>
2023-03-15 17:33:39 +0800

    update

--- src/main/scala/name/spade5/main.scala
+++ src/main/scala/name/spade5/main.scala
@@ -5,16 +5,21 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("FileWordCount").setMaster("local[*]")
-    val ssc = new StreamingContext(conf, Seconds(5))
+    val conf = new SparkConf().setAppName("WordCount").setMaster("yarn-client")
+    conf.set("spark.executor.instances", "3")
+    conf.set("spark.executor.cores", "1")
+    conf.set("spark.streaming.concurrentJobs", "1")
 
-    val lines = ssc.textFileStream("/workspace/input/")
+    val ssc = new StreamingContext(conf, Seconds(10))
+
+    val lines = ssc.socketTextStream("hadoop1", 9999).repartition(1)
+
     val words = lines.flatMap(_.split(" "))
     val wordCounts = words.map((_, 1)).reduceByKey(_ + _)
 
     wordCounts.foreachRDD(rdd => {
       if (!rdd.isEmpty()) {
-        rdd.saveAsTextFile("/workspace/output/" + System.currentTimeMillis() + ".txt")
+        rdd.saveAsTextFile("/workspace/output/" + System.currentTimeMillis())
       }
     })
 
